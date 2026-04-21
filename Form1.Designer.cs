ae799e2cab96ad6b3e99526ccc7f0aa4a2c501e3
shadab75420 <108717957+shadab75420@users.noreply.github.com>
2026-04-21 21:27:54 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Stopwatch2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblMainTime = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnLap = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lstLaps = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblMainTime
+            // 
+            this.lblMainTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainTime.Location = new System.Drawing.Point(254, 88);
+            this.lblMainTime.Name = "lblMainTime";
+            this.lblMainTime.Size = new System.Drawing.Size(300, 80);
+            this.lblMainTime.TabIndex = 0;
+            this.lblMainTime.Text = "00:00:00";
+            this.lblMainTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnStart.Location = new System.Drawing.Point(128, 186);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 57);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.Red;
+            this.btnReset.Location = new System.Drawing.Point(343, 186);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(129, 57);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnLap
+            // 
+            this.btnLap.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLap.ForeColor = System.Drawing.Color.Black;
+            this.btnLap.Location = new System.Drawing.Point(577, 186);
+            this.btnLap.Name = "btnLap";
+            this.btnLap.Size = new System.Drawing.Size(122, 57);
+            this.btnLap.TabIndex = 3;
+            this.btnLap.Text = "Lap";
+            this.btnLap.UseVisualStyleBackColor = false;
+            this.btnLap.Click += new System.EventHandler(this.btnLap_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lstLaps
+            // 
+            this.lstLaps.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lstLaps.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstLaps.ForeColor = System.Drawing.Color.Navy;
+            this.lstLaps.FormattingEnabled = true;
+            this.lstLaps.ItemHeight = 25;
+            this.lstLaps.Location = new System.Drawing.Point(0, 271);
+            this.lstLaps.Name = "lstLaps";
+            this.lstLaps.Size = new System.Drawing.Size(800, 179);
+            this.lstLaps.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstLaps);
+            this.Controls.Add(this.btnLap);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblMainTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMainTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnLap;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox lstLaps;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Stopwatch2
+{
+    public partial class Form1 : Form
+    {
+        int milliseconds = 0;
+        int seconds = 0;
+        int minutes = 0;
+        int lapCount = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            milliseconds += 10;
+
+            if (milliseconds == 1000)
+            {
+                milliseconds = 0;
+                seconds++;
+            }
+
+            if (seconds == 60)
+            {
+                seconds = 0;
+                minutes++;
+            }
+
+            lblMainTime.Text = $"{minutes:D2}:{seconds:D2}:{milliseconds / 10:D2}";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                ((Button)sender).Text = "Start";
+            }
+            else
+            {
+                timer1.Start();
+                ((Button)sender).Text = "Stop";
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            milliseconds = 0;
+            seconds = 0;
+            minutes = 0;
+
+            lblMainTime.Text = "00:00:00";
+        }
+
+        private void btnLap_Click(object sender, EventArgs e)
+        {
+            lstLaps.Items.Add("Lap " + lapCount++ + " - " + lblMainTime.Text);
+        }
+    }
+}
